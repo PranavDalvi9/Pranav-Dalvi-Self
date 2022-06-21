20b0c86fadd349c05b38d2a4224f84853840f9cd
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-06-21 13:07:09 +0530

    Vovels

---
+++ DSA_Practice/Vovels.js
@@ -0,0 +1,19 @@
+const str = "hello";
+
+checkVovel(str);
+
+function checkVovel(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      count++;
+    }
+  }
+  console.log("vovels are", count);
+}
