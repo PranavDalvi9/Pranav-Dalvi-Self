4dad6e2b6fe3fcf317034e19c05d5bc774aab342
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-06-21 13:21:17 +0530

    minimum number

--- DSA_Practice/Minimum.js
+++ DSA_Practice/Minimum.js
@@ -1,13 +1,13 @@
 const arr = [100, 10, 22, 42, 22];
 
-minimum(arr)
+minimum(arr);
 
-function minimum(arr){
-    var min = arr[0]
-    for(let i=0; i<arr.length; i++){
-        if(arr[i] < min){
-            min = arr[i]
-        }
+function minimum(arr) {
+  var min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
     }
-    console.log(min)
+  }
+  console.log(min);
 }
